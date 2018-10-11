c4299c1830fed5403182b62899b815616cdea27d
qarawlus <44025202+qarawlus@users.noreply.github.com>
2018-10-11 23:21:14 +0200

    Update IOFunctions.py
    Adding simple comments to the functions

--- PContacts/IOFunctions.py
+++ PContacts/IOFunctions.py
@@ -1,16 +1,19 @@
+# Import of PyYAML Package
 import yaml
 
+# Loading YAML Data File, file mode is "R"
 def load_yaml(file_path):
 	f = open(file_path, "r")
 	yaml_data = yaml.load(f)
 	f.close()
 	return yaml_data
-
+# Writing to YAML file, file mode is "A" to append to, and not erase the file
 def write_yaml(file_path,data):
 	f = open(file_path,"a")
 	yaml.dump(data,f,default_flow_style=False)
 	f.close()
 
+# Show the details of a contact in a formatted ouput
 def show(name,data):
 	if name in data.keys():
 		output = "Name: "+name+"\n\rEmail: "+data[name]['EMAIL']+"\r\nPhone: "+data[name]['PHONE']
@@ -19,12 +22,12 @@ def show(name,data):
 
 	return output
 
-
+# Add a contact to the YAML file
 def add(name,number,email,file):
 	contact = {name:{"PHONE": number, "EMAIL": email}}
 	write_yaml(file,contact)
 
-
+# Show all contacts in a formatted output
 def list(data):
 	counter = 1
 	output = ""
