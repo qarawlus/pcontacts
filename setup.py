948e8e69e2f50f8157d3d34b5cea24b6bf2086b9
qarawlus <44025202+qarawlus@users.noreply.github.com>
2018-10-11 22:31:50 +0200

    Create setup.py
    Create a simple package setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup 
+
+setup(
+  name="PContacts",
+  version="1.0dev",
+  packages=['PContacts'],
+  long_description="This module is developed to view and store contacts book entries to a YAML file",
+)
+
